15afa1912f27a395843752069986202cecc33db7
Littlenate2114 <littlenate2114@gmail.com>
2016-12-28 13:24:13 +1000

    updated js file

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-$("#thep").hide();
 $(function() {
+  $("#thep").hide();
   $("#fade").click(function(){
-    $("#thep").slideToggle();
+    $("#thep").toggle("800", function(){ //removing paragraph and at same time removing opacity
+            opacity: '0'
+        });
     });
+
+      $(".footer").click(function() {
+        if (window.confirm('would you like to be re-directed to wishingwelldevelopments.com'))
+        {
+            window.open("https://www.google.com");
+        }
+        else{}
+      });
+
 });
